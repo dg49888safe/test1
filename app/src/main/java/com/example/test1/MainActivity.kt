d9396711e6f45d1f0e92e8a68f677d2a2040ee31
dg49888safe <dg49888safe@gmail.com>
2024-12-10 20:55:12 +0800

    20241211

---
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.test1
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.test1.ui.theme.Test1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 启动 ClipboardMonitorService 服务
+        val serviceIntent = Intent(this, ClipboardMonitorService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent) // Android 8.0+ 前台服务
+        } else {
+            startService(serviceIntent)
+        }
+
+        // 设置 Compose UI
+        enableEdgeToEdge()
+        setContent {
+            Test1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "粘板trc20、 20241211!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Test1Theme {
+        Greeting("Android")
+    }
+}
